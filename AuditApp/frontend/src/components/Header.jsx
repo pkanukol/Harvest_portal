cb89e4e0b06efb51c89d1a1473fbfc98a61347f2
Pavani Kanukollu <59699112+pkanukol@users.noreply.github.com>
2026-07-29 14:10:50 +0530

    Remove logo image and logout button from Header
    Removed logo image and logout button from header.

--- AuditApp/frontend/src/components/Header.jsx
+++ AuditApp/frontend/src/components/Header.jsx
@@ -5,7 +5,6 @@ export default function Header({ user, showDashboardNav, onDashboard, showSpaNav
     <div className="hdr">
       <div className="hdr-inner">
         <div className="hdr-left">
-          <img src="/logo.png" alt="Harvest International School" className="hdr-logo-img" />
           <div className="hdr-sub" style={{ marginTop: 0 }}>{headerSub}</div>
         </div>
         <div className="hdr-right">
@@ -22,9 +21,6 @@ export default function Header({ user, showDashboardNav, onDashboard, showSpaNav
               &#127942; SPA Observation
             </button>
           )}
-          <button className="btn btn-logout" onClick={onLogout}>
-            &#9211; Logout
-          </button>
         </div>
       </div>
     </div>
